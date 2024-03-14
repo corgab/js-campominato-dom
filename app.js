be092fc3455c308f5afb6f74ee49b75676aaaabf
gab <corbanigabriele@gmail.com>
2024-03-14 18:21:05 +0100

    ADD: esercizio (bandiera bianca)

--- app.js
+++ app.js
@@ -4,67 +4,70 @@
 //console con numero casella
 
 
-const buttonElement  = document.getElementById("avvio")
+const buttonElement = document.getElementById("avvio")
 const avvioPartita = document.querySelector(".grid-wrapper")
 const gridElement = document.querySelector(".grid")
 
 const size = 10
-const numeroCelle = size * size
+const numeroCelle = size * size;
 
 //avviare la partita aggiungendo il grid e togliendo il bottone
-buttonElement.addEventListener("click", function() {
+buttonElement.addEventListener("click", function () {
     avvioPartita.classList.remove("d-none")
     buttonElement.classList.add("d-none")
 })
 
+//bombe casuali uniche
+const bombe = []
+
+while (bombe.length < 16) {
+    const randomNumber = numeriRandom()
+
+    if (bombe.includes(randomNumber) === false) {
+        bombe.push(randomNumber)
+    }
+}
+
+console.log(bombe);
+
 //ciclo for per generare le caselle
-for(let i = 0; i < numeroCelle; i++ )
-{
+for (let i = 0; i < numeroCelle; i++) {
+
     const num = i + 1
+
 
     const cella = document.createElement("div")
     cella.classList.add("cell")
+    
     cella.innerHTML = num
 
     gridElement.append(cella)
 
-    cella.addEventListener("click", function() {
+    cella.addEventListener("click", function () {
         console.log("hai cliccato sulla casella " + num)
-        cella.classList.toggle("bg-orange")
+        let punteggio = 0
+        if (bombe.includes(num)) {
+            cella.classList.add("bomb")
+            punteggio++
+        } else {
+            cella.classList.add("bg-orange")
+            punteggio++
+            
+        }
+        console.log(punteggio)
+        
     })
     
 }
 
-//generare 16 numeri casuali diversi
-//bombe casuali
-const bombe = []
-
-while (bombe.length < 16) {
-    // generare numeri casuali
-    const num = numeriRandom()
-    if(num == bombe[num]) {
-        numeriRandom()
-    } else {
-        bombe.push(num)
-    }
-
-    //togliere numeri doppi
-    // if (num === bombe[num] {
-
-    // }
-    console.log(bombe[num])
-
-}
-
-
 
 function numeriRandom() {
-    const numeriCasuali = Math.floor(Math.random() * numeroCelle +1)
-
-    return numeriCasuali
+    const numeroCasuale = Math.floor(Math.random() * numeroCelle + 1)
+    return numeroCasuale
 }
 
-console.log(bombe)
+
+
 
 
 
